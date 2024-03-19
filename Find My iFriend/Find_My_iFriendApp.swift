a15627f717b55df54d4b3279f514f47b5ac86685
theodevoid <voidfnc9@gmail.com>
2024-03-19 14:57:36 +0700

    Initial Commit

---
+++ Find My iFriend/Find_My_iFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_iFriendApp.swift
+//  Find My iFriend
+//
+//  Created by Theodore Mangowal on 19/03/24.
+//
+
+import SwiftUI
+
+@main
+struct Find_My_iFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
